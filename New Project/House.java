66f4e1f68e2285c720eb02e15121f15b586ed8ac
rafianajam22 <rafia.najam@gmail.com>
2015-09-23 16:50:21 -0700

    Created by Coding Ground Client

---
+++ New Project/House.java
@@ -0,0 +1,6 @@
+public interface House {
+    public int getRooms(int houseId);
+    public int getBath(int houseId);
+    public int getTotalArea(int houseId);
+    public int getTotalCoveredArea(int houseId);
+}
