40ef947c0e34091ba505636cbfd070512e137ecd
rafianajam22 <rafia.najam@gmail.com>
2015-09-24 14:50:43 -0700

    Created by Coding Ground Client

---
+++ New Project/HouseCount.java
@@ -0,0 +1,17 @@
+public class HouseCount
+{
+    public static void main(String args[])
+    {
+        
+    
+    HouseImpl houseImpl=new HouseImpl();
+    for(int i=0,j=0;i<=2 && j<=2;i++)
+    {
+        new HouseImple();
+        j++;
+        
+    }
+    
+    System.out.println(j);
+    }
+}
